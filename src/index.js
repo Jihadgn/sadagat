f0cf0f75f9d5519ee73a63d60a50d5c6acd960d6
jihadgn <jehad987gn@gmail.com>
2024-01-22 23:36:16 +0200

    setting react framework

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import ReactDOM from 'react-dom/client';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const myFirstElement = <h1>Hello React!</h1>
-
-root.render(myFirstElement);
